2348bc47a6ecc04f45f4644e07d4e9db755a1762
thitipongr <thitipong.r.psn@gmail.com>
2023-12-20 23:46:22 +0700

    get requirement

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+
+  // โจทย์: สร้างเว็บแอปพลิเคชั่นสำหรับการจัดการข้อมูลส่วนตัว (Personal Information Management App)
+  // วัตถุประสงค์: แอปพลิเคชันนี้จะช่วยให้ผู้ใช้สามารถบันทึกและจัดการข้อมูลส่วนตัวได้ เช่น กำหนดการ, บันทึกประจำวัน, รายการสิ่งที่ต้องทำ ฯลฯ
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    </main>
+  )
+}
