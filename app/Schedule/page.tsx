c2818381c2db2d89973cf4c774aada6bcea1c35f
thitipongr <thitipong.r.psn@gmail.com>
2023-12-28 22:36:10 +0700

    try fullcalendar component

--- app/Schedule/page.tsx
+++ app/Schedule/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+
+import FullCalendar from "@fullcalendar/react";
+import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const page = () => {
   return (
-    <div>Schedule</div>
-  )
-}
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        weekends={false}
+        events={[
+          { title: "event 1", date: "2019-04-01" },
+          { title: "event 2", date: "2019-04-02" },
+        ]}
+      />
+    </div>
+  );
+};
 
-export default page
+export default page;
