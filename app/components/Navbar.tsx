0e8de94cabf42f64037083bc8cf77e070b3fe4ef
thitipongr <thitipong.r.psn@gmail.com>
2024-01-05 23:28:15 +0700

    Draft search field

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -48,6 +48,23 @@ const Navbar = () => {
               </Link>
             );
           })}
+          <div className="rounded-lg border mx-3 py-2 px-3 w-52 flex flex-row">
+            <input type="text" className="w-full focus:outline-none" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+          </div>
           <Image
             src={"https://avatars.githubusercontent.com/u/144009672?v=4"}
             alt="Thitipong"
