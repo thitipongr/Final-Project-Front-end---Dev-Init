f400bda3266c932bcb267efb6b5a3339fe57772c
thitipongr <thitipong.r.psn@gmail.com>
2024-01-07 22:46:51 +0700

    Navbar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -19,9 +19,25 @@ const pageList = [
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center border">
+    <div className="flex justify-center border px-5">
       <div className="flex flex-row justify-between items-center h-[60px] container">
-        <div>
+        <div className="flex sm:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+        <div className="hidden sm:flex">
           <Link href={"/"} className="flex flex-row">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -40,31 +56,50 @@ const Navbar = () => {
             <div className="pl-1">Personal Information Management</div>
           </Link>
         </div>
+        <div className="flex sm:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"
+            />
+          </svg>
+          <div className="pl-1">PIM</div>
+        </div>
         <div className="flex flex-row items-center">
-          {pageList.map((data, index) => {
-            return (
-              <Link key={index} href={data.pathName} className="mx-1 px-2">
-                {data.displayName}
-              </Link>
-            );
-          })}
-          <button className="rounded-lg border mx-3 py-2 px-3 w-52 flex flex-row hover:bg-gray-100">
-            <div className="w-full text-left">Quick search...</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              />
-            </svg>
-          </button>
+          <div className="sm:flex flex-row items-center hidden">
+            {pageList.map((data, index) => {
+              return (
+                <Link key={index} href={data.pathName} className="mx-1 px-2">
+                  {data.displayName}
+                </Link>
+              );
+            })}
+            <button className="rounded-lg border mx-3 py-2 px-3 w-52 flex flex-row hover:bg-gray-100">
+              <div className="w-full text-left">Quick search...</div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </button>
+          </div>
           <Image
             src={"https://avatars.githubusercontent.com/u/144009672?v=4"}
             alt="Thitipong"
