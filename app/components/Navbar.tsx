90a5f13f9464582ac58b7f19ef8a0f08d56b45d7
thitipongr <thitipong.r.psn@gmail.com>
2024-01-15 22:52:36 +0700

    add theme swicth button but not change yet

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -38,6 +38,13 @@ const Navbar = () => {
       : setExpandProfileState(false);
   };
 
+  const [darkModeSwicth, setDarkModeSwicth] = useState(false);
+  const darkModeSwicthHandler = () => {
+    darkModeSwicth === false
+      ? setDarkModeSwicth(true)
+      : setDarkModeSwicth(false);
+  };
+
   return (
     <div className="flex justify-center border-b z-10">
       <div className="flex flex-row justify-between items-center h-[60px] container">
@@ -169,7 +176,8 @@ const Navbar = () => {
             </div>
           </div>
           <div className="relative">
-            <Image onClick={expandProfileHandler}
+            <Image
+              onClick={expandProfileHandler}
               src={"https://avatars.githubusercontent.com/u/144009672?v=4"}
               alt="Thitipong"
               width={40}
@@ -207,12 +215,35 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6"
+                className={clsx("w-6 h-6", {
+                  hidden: darkModeSwicth,
+                  flex : !darkModeSwicth,
+                })}
+                onClick={darkModeSwicthHandler}
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={clsx("w-6 h-6", {
+                  hidden: !darkModeSwicth,
+                  flex : darkModeSwicth,
+                })}
+                onClick={darkModeSwicthHandler}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
                 />
               </svg>
 
