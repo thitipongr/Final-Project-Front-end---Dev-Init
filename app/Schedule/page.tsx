6e8718c2f31676caf9c20e2471748a7386603424
thitipongr <thitipong.r.psn@gmail.com>
2024-01-16 23:11:56 +0700

    Change layout of calendar

--- app/Schedule/page.tsx
+++ app/Schedule/page.tsx
@@ -3,13 +3,18 @@
 import FullCalendar from "@fullcalendar/react";
 import React from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 const Page = () => {
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
-      height={"100%"}
+      plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+      headerToolbar={{
+        left: "prev,next today",
+        center: "title",
+        right: "timeGridWeek,dayGridMonth",
+      }}
       events={[
         { title: "event 1", date: "2024-01-01" },
         { title: "event 2", date: "2024-01-01" },
@@ -19,6 +24,7 @@ const Page = () => {
       droppable={true}
       selectable={true}
       selectMirror={true}
+      height={"100%"}
     />
   );
 };
