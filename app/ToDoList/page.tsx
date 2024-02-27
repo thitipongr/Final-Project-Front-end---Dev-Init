c8624692d6195b391c4778562c665b8020a8483f
thitipongr <thitipong.r.psn@gmail.com>
2024-02-27 17:34:37 +0700

    Fixed margin when open madal  of ToDo

--- app/ToDoList/page.tsx
+++ app/ToDoList/page.tsx
@@ -48,173 +48,175 @@ const Page = () => {
   });
 
   return (
-    <div className="flex flex-col h-full space-y-2">
-      <button
-        className="w-full bg-gray-200 rounded-lg p-2 font-bold"
-        onClick={() => setShowAddingModal(true)}
-      >
-        Add ToDo
-      </button>
-      <div className="flex h-full justify-between space-x-2">
-        <div className="w-full rounded-lg border">
-          <div className="border-b px-2 py-2 bg-gray-100 font-bold text-center">
-            ToDo
-          </div>
-          <div className="p-2 space-y-2">
-            {Object.keys(toDoTesks[0] || {}).length
-              ? toDoTesks.map(
-                  (
-                    toDoTesks: {
-                      id?: string;
-                      title?: string;
-                      dueDateState?: boolean;
-                      dueDate?: string;
-                      description?: string;
-                      teskState?: string;
-                      archive?: boolean;
-                    },
-                    index: number
-                  ) => {
-                    if (toDoTesks.teskState === "ToDo")
-                      return (
-                        <div
-                          key={index}
-                          className="w-full rounded-lg border"
-                          onClick={() => {
-                            const packData = {
-                              id: toDoTesks.id || "",
-                              title: toDoTesks.title || "",
-                              dueDateState: toDoTesks.dueDateState || false,
-                              dueDate: toDoTesks.dueDate || "",
-                              description: toDoTesks.description || "",
-                              teskState: toDoTesks.teskState || "",
-                              archive: toDoTesks.archive || false,
-                            };
-
-                            setSendDataToShowModal(packData);
-                            setShowDetailModal(true);
-                          }}
-                        >
+    <>
+      <div className="flex flex-col h-full space-y-2">
+        <button
+          className="w-full bg-gray-200 rounded-lg p-2 font-bold"
+          onClick={() => setShowAddingModal(true)}
+        >
+          Add ToDo
+        </button>
+        <div className="flex h-full justify-between space-x-2">
+          <div className="w-full rounded-lg border">
+            <div className="border-b px-2 py-2 bg-gray-100 font-bold text-center">
+              ToDo
+            </div>
+            <div className="p-2 space-y-2">
+              {Object.keys(toDoTesks[0] || {}).length
+                ? toDoTesks.map(
+                    (
+                      toDoTesks: {
+                        id?: string;
+                        title?: string;
+                        dueDateState?: boolean;
+                        dueDate?: string;
+                        description?: string;
+                        teskState?: string;
+                        archive?: boolean;
+                      },
+                      index: number
+                    ) => {
+                      if (toDoTesks.teskState === "ToDo")
+                        return (
                           <div
-                            className={clsx(
-                              "rounded-lg p-2 bg-gray-100 font-bold truncate",
-                              {
-                                "border-b rounded-b-none":
+                            key={index}
+                            className="w-full rounded-lg border"
+                            onClick={() => {
+                              const packData = {
+                                id: toDoTesks.id || "",
+                                title: toDoTesks.title || "",
+                                dueDateState: toDoTesks.dueDateState || false,
+                                dueDate: toDoTesks.dueDate || "",
+                                description: toDoTesks.description || "",
+                                teskState: toDoTesks.teskState || "",
+                                archive: toDoTesks.archive || false,
+                              };
+
+                              setSendDataToShowModal(packData);
+                              setShowDetailModal(true);
+                            }}
+                          >
+                            <div
+                              className={clsx(
+                                "rounded-lg p-2 bg-gray-100 font-bold truncate",
+                                {
+                                  "border-b rounded-b-none":
+                                    toDoTesks.description !== "" ||
+                                    toDoTesks.dueDateState,
+                                }
+                              )}
+                            >
+                              {toDoTesks.title}
+                            </div>
+                            <div
+                              className={clsx("w-full space-y-2", {
+                                "p-2":
                                   toDoTesks.description !== "" ||
                                   toDoTesks.dueDateState,
-                              }
-                            )}
-                          >
+                              })}
+                            >
+                              {toDoTesks.description !== "" ? (
+                                <div className="truncate">
+                                  {toDoTesks.description}
+                                </div>
+                              ) : null}
+                              {toDoTesks.dueDateState ? (
+                                <div
+                                  className={clsx(
+                                    "truncate p-2 w-min rounded-lg",
+                                    {
+                                      "bg-red-300 border-red-600":
+                                        new Date(
+                                          toDoTesks.dueDate || 0
+                                        ).getTime() <= new Date().getTime(),
+                                      "bg-yellow-200 border-yellow-500":
+                                        new Date(
+                                          toDoTesks.dueDate || 0
+                                        ).getTime() > new Date().getTime() &&
+                                        new Date(
+                                          toDoTesks.dueDate || 0
+                                        ).getTime() <
+                                          new Date().getTime() + 86400000,
+                                      "bg-green-200 border-green-500":
+                                        new Date(
+                                          toDoTesks.dueDate || 0
+                                        ).getTime() >
+                                        new Date().getTime() + 86400000,
+                                    }
+                                  )}
+                                >
+                                  {new Date(toDoTesks.dueDate || 0)
+                                    .toLocaleString("en-US")
+                                    .replace(":00", "")}
+                                </div>
+                              ) : null}
+                            </div>
+                          </div>
+                        );
+                    }
+                  )
+                : null}
+            </div>
+          </div>
+          <div className="w-full rounded-lg border">
+            <div className="border-b px-2 py-2 bg-gray-100 font-bold text-center">
+              Doing
+            </div>
+            <div className="p-2 space-y-2">
+              {Object.keys(toDoTesks[0] || {}).length
+                ? toDoTesks.map(
+                    (
+                      toDoTesks: {
+                        id?: string;
+                        title?: string;
+                        dueDateState?: boolean;
+                        dueDate?: string;
+                        description?: string;
+                        teskState?: string;
+                        archive?: boolean;
+                      },
+                      index: number
+                    ) => {
+                      if (toDoTesks.teskState === "Doing")
+                        return (
+                          <div className="" key={index}>
                             {toDoTesks.title}
                           </div>
-                          <div
-                            className={clsx("w-full space-y-2", {
-                              "p-2":
-                                toDoTesks.description !== "" ||
-                                toDoTesks.dueDateState,
-                            })}
-                          >
-                            {toDoTesks.description !== "" ? (
-                              <div className="truncate">
-                                {toDoTesks.description}
-                              </div>
-                            ) : null}
-                            {toDoTesks.dueDateState ? (
-                              <div
-                                className={clsx(
-                                  "truncate p-2 w-min rounded-lg",
-                                  {
-                                    "bg-red-300 border-red-600":
-                                      new Date(
-                                        toDoTesks.dueDate || 0
-                                      ).getTime() <= new Date().getTime(),
-                                    "bg-yellow-200 border-yellow-500":
-                                      new Date(
-                                        toDoTesks.dueDate || 0
-                                      ).getTime() > new Date().getTime() &&
-                                      new Date(
-                                        toDoTesks.dueDate || 0
-                                      ).getTime() <
-                                        new Date().getTime() + 86400000,
-                                    "bg-green-200 border-green-500":
-                                      new Date(
-                                        toDoTesks.dueDate || 0
-                                      ).getTime() >
-                                      new Date().getTime() + 86400000,
-                                  }
-                                )}
-                              >
-                                {new Date(toDoTesks.dueDate || 0)
-                                  .toLocaleString("en-US")
-                                  .replace(":00", "")}
-                              </div>
-                            ) : null}
+                        );
+                    }
+                  )
+                : null}
+            </div>
+          </div>
+          <div className="w-full rounded-lg border">
+            <div className="border-b px-2 py-2 bg-gray-100 font-bold text-center">
+              Done
+            </div>
+            <div className="p-2 space-y-2">
+              {Object.keys(toDoTesks[0] || {}).length
+                ? toDoTesks.map(
+                    (
+                      toDoTesks: {
+                        id?: string;
+                        title?: string;
+                        dueDateState?: boolean;
+                        dueDate?: string;
+                        description?: string;
+                        teskState?: string;
+                        archive?: boolean;
+                      },
+                      index: number
+                    ) => {
+                      if (toDoTesks.teskState === "Done")
+                        return (
+                          <div className="" key={index}>
+                            {toDoTesks.title}
                           </div>
-                        </div>
-                      );
-                  }
-                )
-              : null}
-          </div>
-        </div>
-        <div className="w-full rounded-lg border">
-          <div className="border-b px-2 py-2 bg-gray-100 font-bold text-center">
-            Doing
-          </div>
-          <div className="p-2 space-y-2">
-            {Object.keys(toDoTesks[0] || {}).length
-              ? toDoTesks.map(
-                  (
-                    toDoTesks: {
-                      id?: string;
-                      title?: string;
-                      dueDateState?: boolean;
-                      dueDate?: string;
-                      description?: string;
-                      teskState?: string;
-                      archive?: boolean;
-                    },
-                    index: number
-                  ) => {
-                    if (toDoTesks.teskState === "Doing")
-                      return (
-                        <div className="" key={index}>
-                          {toDoTesks.title}
-                        </div>
-                      );
-                  }
-                )
-              : null}
-          </div>
-        </div>
-        <div className="w-full rounded-lg border">
-          <div className="border-b px-2 py-2 bg-gray-100 font-bold text-center">
-            Done
-          </div>
-          <div className="p-2 space-y-2">
-            {Object.keys(toDoTesks[0] || {}).length
-              ? toDoTesks.map(
-                  (
-                    toDoTesks: {
-                      id?: string;
-                      title?: string;
-                      dueDateState?: boolean;
-                      dueDate?: string;
-                      description?: string;
-                      teskState?: string;
-                      archive?: boolean;
-                    },
-                    index: number
-                  ) => {
-                    if (toDoTesks.teskState === "Done")
-                      return (
-                        <div className="" key={index}>
-                          {toDoTesks.title}
-                        </div>
-                      );
-                  }
-                )
-              : null}
+                        );
+                    }
+                  )
+                : null}
+            </div>
           </div>
         </div>
       </div>
@@ -242,7 +244,7 @@ const Page = () => {
           setToDoTesks={setToDoTesks}
         />
       ) : null}
-    </div>
+    </>
   );
 };
 
