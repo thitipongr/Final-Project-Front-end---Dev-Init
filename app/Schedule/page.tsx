144adebb2c9265270a94051cda0011f2617f5362
thitipongr <thitipong.r.psn@gmail.com>
2024-03-12 22:41:22 +0700

    Adjust dark mode to Schedule page

--- app/Schedule/page.tsx
+++ app/Schedule/page.tsx
@@ -80,51 +80,53 @@ const Page = () => {
 
   return (
     <div className="inline">
-      <FullCalendar
-        ref={calendarRef}
-        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-        initialView={defaultView}
-        headerToolbar={{
-          left: "title",
-          center: "",
-          right: "today",
-        }}
-        footerToolbar={{
-          left: "dayGridMonth,timeGridWeek,timeGridDay",
-          center: "",
-          right: "prev,next",
-        }}
-        titleFormat={{
-          year: "2-digit",
-          month: "short",
-          day: "2-digit",
-        }}
-        buttonText={{
-          today: "TODAY",
-          month: "Month",
-          week: "Week",
-          day: "Day",
-        }}
-        select={handleDateSelect}
-        events={calendarEvents}
-        eventTimeFormat={{
-          hour: "numeric",
-          minute: "2-digit",
-          meridiem: "short",
-        }}
-        nowIndicator={true}
-        selectable={true}
-        height={"100%"}
-        windowResize={() => {
-          let calendarApi = calendarRef.current.getApi();
-          window.innerWidth <= 768
-            ? calendarApi.changeView("timeGridDay")
-            : calendarApi.changeView("dayGridMonth");
-          calendarApi.view.calendar;
-        }}
-        dayMaxEvents
-        eventClick={handleEventSelect}
-      />
+      <div className="h-full dark:text-white">
+        <FullCalendar
+          ref={calendarRef}
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          initialView={defaultView}
+          headerToolbar={{
+            left: "title",
+            center: "",
+            right: "today",
+          }}
+          footerToolbar={{
+            left: "dayGridMonth,timeGridWeek,timeGridDay",
+            center: "",
+            right: "prev,next",
+          }}
+          titleFormat={{
+            year: "2-digit",
+            month: "short",
+            day: "2-digit",
+          }}
+          buttonText={{
+            today: "TODAY",
+            month: "Month",
+            week: "Week",
+            day: "Day",
+          }}
+          select={handleDateSelect}
+          events={calendarEvents}
+          eventTimeFormat={{
+            hour: "numeric",
+            minute: "2-digit",
+            meridiem: "short",
+          }}
+          nowIndicator={true}
+          selectable={true}
+          height={"100%"}
+          windowResize={() => {
+            let calendarApi = calendarRef.current.getApi();
+            window.innerWidth <= 768
+              ? calendarApi.changeView("timeGridDay")
+              : calendarApi.changeView("dayGridMonth");
+            calendarApi.view.calendar;
+          }}
+          dayMaxEvents
+          eventClick={handleEventSelect}
+        />
+      </div>
       {showAddingModal ? (
         <AddEventModal
           setShowModal={setShowAddingModal}
