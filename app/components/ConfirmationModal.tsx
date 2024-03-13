0b698c03c60e575f454852e837851b9f3c7e0c7b
thitipongr <thitipong.r.psn@gmail.com>
2024-03-13 21:27:12 +0700

    Adjust dark mode to Delete confirm modal

--- app/components/ConfirmationModal.tsx
+++ app/components/ConfirmationModal.tsx
@@ -17,7 +17,7 @@ const ConfirmationModal = ({
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-[300px]">
-          <div className="border-0 rounded-lg relative flex flex-col w-full min-h-[200px] bg-white outline-none focus:outline-none">
+          <div className="border-0 rounded-lg relative flex flex-col w-full min-h-[200px] bg-white outline-none focus:outline-none dark:bg-slate-900">
             <div className="flex-none bg-red-500 rounded-t-lg px-2 py-3 text-white font-bold uppercase">
               {`Delete ${confirmationFrom}`}
             </div>
