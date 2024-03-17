1945fbdecf8ac4fe265b8836a72af6c0a67e5dc4
thitipongr <thitipong.r.psn@gmail.com>
2024-03-17 18:24:06 +0700

    Shrinking profile menu when click other element but profileMenu

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
 
   const [expandProfileState, setExpandProfileState] = useState(false);
   const expandProfileHandler = () => {
-    expandProfileState === false
+    !expandProfileState
       ? setExpandProfileState(true)
       : setExpandProfileState(false);
   };
@@ -59,6 +59,7 @@ const Navbar = () => {
         setTheme("light");
       }
     }
+    setExpandProfileState(true);
   };
 
   useEffect(() => {
@@ -77,9 +78,18 @@ const Navbar = () => {
   const [searchModal, setSearchModal] = useState(false);
   const router = useRouter();
 
+  if (typeof window !== "undefined") {
+    var myProfileMenu = document.getElementById("profileMenu");
+    document.body.addEventListener("click", (event) => {
+      if (!myProfileMenu?.contains(event.target as Node)) {
+        setExpandProfileState(false);
+      }
+    });
+  }
+
   return (
     <div className="flex justify-center border-b z-10 lg:px-7 dark:text-neutral-50 select-none">
-      <div className="flex flex-row justify-between items-center h-[60px] container">
+      <div className="flex flex-row justify-between items-center h-[60px] container z-10">
         <div
           className={clsx(
             "flex lg:hidden w-[60px] items-center justify-center h-full",
@@ -224,8 +234,9 @@ const Navbar = () => {
               className="rounded-full z-50 lg:ml-3"
             />
             <div
+              id="profileMenu"
               className={clsx(
-                "items-center w-10 py-5 pt-12 space-y-7 lg:ml-3 dark:bg-slate-800",
+                "items-center w-10 py-5 pt-12 lg:ml-3 dark:bg-slate-800",
                 {
                   hidden: !expandProfileState,
                   "flex flex-col fixed top-[30px] bg-gray-200 rounded-b-full -z-10":
@@ -233,38 +244,89 @@ const Navbar = () => {
                 }
               )}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 mt-3"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-
-              <div>
+              <div className="space-y-7">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className={clsx("w-6 h-6", {
-                    hidden: theme !== "light",
-                    flex: theme === "light",
-                  })}
-                  onClick={darkModeSwicthHandler}
+                  className="w-6 h-6 mt-3"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={clsx("w-6 h-6", {
+                      hidden: theme !== "light",
+                      flex: theme === "light",
+                    })}
+                    onClick={darkModeSwicthHandler}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={clsx("w-6 h-6", {
+                      hidden: theme !== "dark",
+                      flex: theme === "dark",
+                    })}
+                    onClick={darkModeSwicthHandler}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={clsx("w-6 h-6", {
+                      hidden: theme !== "",
+                      flex: theme === "",
+                    })}
+                    onClick={darkModeSwicthHandler}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+                    />
+                  </svg>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
                   />
                 </svg>
                 <svg
@@ -273,67 +335,15 @@ const Navbar = () => {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className={clsx("w-6 h-6", {
-                    hidden: theme !== "dark",
-                    flex: theme === "dark",
-                  })}
-                  onClick={darkModeSwicthHandler}
+                  className="w-6 h-6"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-                  />
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={clsx("w-6 h-6", {
-                    hidden: theme !== "",
-                    flex: theme === "",
-                  })}
-                  onClick={darkModeSwicthHandler}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+                    d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
                   />
                 </svg>
               </div>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
-                />
-              </svg>
             </div>
           </div>
         </div>
--- app/components/Navbar/SearchModal.tsx
+++ app/components/Navbar/SearchModal.tsx
@@ -34,33 +34,46 @@ const SearchModal = ({
 
   const router = useRouter();
   return (
-    <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 outline-none focus:outline-none">
-        <div className="relative w-[315px] z-50">
-          <div className="border-0 rounded-lg relative flex flex-col w-full h-[400px] bg-white dark:bg-slate-900 dark:text-white outline-none focus:outline-none">
-            <div
-              className={clsx("relative p-2 flex flex-col space-y-2 h-full")}
-            >
-              <div className="flex flex-col space-y-2">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className={clsx(
-                    "w-full py-1 border-b focus:outline-none focus:border-cyan-900 dark:bg-slate-900 px-1"
-                  )}
-                  value={searchTxt}
-                  onChange={(e) => {
-                    setSearchTxt(e.target.value);
-                  }}
-                  autoFocus
-                />
-              </div>
-              <div className="flex-1 relative">
-                {Object.keys(toDoTesks[0] || {}).length ? (
-                  <div className="flex flex-col absolute h-full w-full space-y-2 overflow-y-auto">
-                    {toDoTesks
-                      .filter(
-                        (data: {
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 outline-none focus:outline-none z-10">
+      <div className="relative w-[315px] z-50">
+        <div className="border-0 rounded-lg relative flex flex-col w-full h-[400px] bg-white dark:bg-slate-900 dark:text-white outline-none focus:outline-none">
+          <div className={clsx("relative p-2 flex flex-col space-y-2 h-full")}>
+            <div className="flex flex-col space-y-2">
+              <input
+                type="text"
+                placeholder="Search..."
+                className={clsx(
+                  "w-full py-1 border-b focus:outline-none focus:border-cyan-900 dark:bg-slate-900 px-1"
+                )}
+                value={searchTxt}
+                onChange={(e) => {
+                  setSearchTxt(e.target.value);
+                }}
+                autoFocus
+              />
+            </div>
+            <div className="flex-1 relative">
+              {Object.keys(toDoTesks[0] || {}).length ? (
+                <div className="flex flex-col absolute h-full w-full space-y-2 overflow-y-auto">
+                  {toDoTesks
+                    .filter(
+                      (data: {
+                        id?: string;
+                        title?: string;
+                        dueDateState?: boolean;
+                        dueDate?: string;
+                        description?: string;
+                        teskState?: string;
+                        archive?: boolean;
+                      }) => {
+                        return (data.title || "")
+                          .toLowerCase()
+                          .includes(searchTxt.toLowerCase());
+                      }
+                    )
+                    .map(
+                      (
+                        toDoTesks: {
                           id?: string;
                           title?: string;
                           dueDateState?: boolean;
@@ -68,68 +81,42 @@ const SearchModal = ({
                           description?: string;
                           teskState?: string;
                           archive?: boolean;
-                        }) => {
-                          return (data.title || "")
-                            .toLowerCase()
-                            .includes(searchTxt.toLowerCase());
-                        }
-                      )
-                      .map(
-                        (
-                          toDoTesks: {
-                            id?: string;
-                            title?: string;
-                            dueDateState?: boolean;
-                            dueDate?: string;
-                            description?: string;
-                            teskState?: string;
-                            archive?: boolean;
-                          },
-                          index
-                        ) => {
-                          return (
-                            <button
-                              key={index}
-                              onClick={() => {
-                                router.push(
-                                  `/ToDoList?hlTodo=${toDoTesks.id}&hlTodoState=${toDoTesks.teskState}`
-                                );
-                                setSearchModal(false);
-                                setExpandMenuState(false);
+                        },
+                        index
+                      ) => {
+                        return (
+                          <button
+                            key={index}
+                            onClick={() => {
+                              router.push(
+                                `/ToDoList?hlTodo=${toDoTesks.id}&hlTodoState=${toDoTesks.teskState}`
+                              );
+                              setSearchModal(false);
+                              setExpandMenuState(false);
+                            }}
+                          >
+                            <ToDoCard
+                              toDoTesks={toDoTesks}
+                              setState={{
+                                setSendDataToShowModal,
+                                setShowDetailModal,
                               }}
-                            >
-                              <ToDoCard
-                                toDoTesks={toDoTesks}
-                                setState={{
-                                  setSendDataToShowModal,
-                                  setShowDetailModal,
-                                }}
-                              />
-                            </button>
-                          );
-                        }
-                      )}
-                  </div>
-                ) : null}
-              </div>
+                            />
+                          </button>
+                        );
+                      }
+                    )}
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
-        <div
-          className="opacity-80 fixed inset-0 z-40 bg-black"
-          onClick={() => setSearchModal(false)}
-        ></div>
       </div>
-      {showDetailModal ? (
-        <ShowToDoEventModal
-          setShowDetailModal={setShowDetailModal}
-          getDataToModal={SendDataToShowModal}
-          defaultCheckId={"ToDo"}
-          toDoTesks={toDoTesks}
-          setToDoTesks={setToDoTesks}
-        />
-      ) : null}
-    </>
+      <div
+        className="opacity-80 fixed inset-0 z-40 bg-black"
+        onClick={() => setSearchModal(false)}
+      ></div>
+    </div>
   );
 };
 
